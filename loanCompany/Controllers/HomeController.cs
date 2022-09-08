57986bf1fdf822d1bf86c64ff6a812912e67cb06
kzish <kudzai@globelise.com>
2022-09-08 23:18:08 +0200

    init commit

---
+++ loanCompany/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using loanCompany.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace loanCompany.Controllers
+{
+    [Route("")]
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("Team")]
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
